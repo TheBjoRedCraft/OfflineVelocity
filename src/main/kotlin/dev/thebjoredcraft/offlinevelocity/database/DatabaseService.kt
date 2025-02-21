3a3574960181ae7f8d3f255739835f0bd99187b3
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-02-22 00:15:43 +0100

    feat: add logging + make async

--- src/main/kotlin/dev/thebjoredcraft/offlinevelocity/database/DatabaseService.kt
+++ src/main/kotlin/dev/thebjoredcraft/offlinevelocity/database/DatabaseService.kt
@@ -1,6 +1,11 @@
 package dev.thebjoredcraft.offlinevelocity.database
 
+import com.velocitypowered.api.proxy.Player
+import dev.thebjoredcraft.offlinevelocity.OfflineVelocity
 import dev.thebjoredcraft.offlinevelocity.player.PlayerData
+import dev.thebjoredcraft.offlinevelocity.plugin
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.ResultSet
@@ -8,9 +13,10 @@ import java.util.UUID
 
 object DatabaseService {
     private lateinit var connection: Connection
+    private val path: String = "${OfflineVelocity.instance.dataDirectory}/player-storage.db"
 
     fun connect() {
-        val url = "jdbc:sqlite:player-storage.db"
+        val url = "jdbc:sqlite:$path"
 
         connection = DriverManager.getConnection(url)
 
@@ -22,47 +28,73 @@ object DatabaseService {
             );
         """
         statement.execute(sql)
+
+        OfflineVelocity.instance.logger.info("OV > Connected to SQLite database.")
     }
 
     suspend fun loadPlayer(uuid: UUID): PlayerData {
-        val sql = "SELECT * FROM players WHERE uuid = ?"
+        withContext(Dispatchers.IO) {
+            val sql = "SELECT * FROM players WHERE uuid = ?"
 
-        val preparedStatement = connection.prepareStatement(sql)
+            val preparedStatement = connection.prepareStatement(sql)
 
-        preparedStatement.setString(1, uuid.toString())
+            preparedStatement.setString(1, uuid.toString())
 
-        val resultSet: ResultSet = preparedStatement.executeQuery()
-        return if (resultSet.next()) {
-            PlayerData(UUID.fromString(resultSet.getString("uuid")), resultSet.getString("name"), true)
-        } else {
-            PlayerData(uuid, "Unknown", false)
+            val resultSet: ResultSet = preparedStatement.executeQuery()
+            return@withContext if (resultSet.next()) {
+                PlayerData(UUID.fromString(resultSet.getString("uuid")), resultSet.getString("name"), true)
+            } else {
+                createNotExistingUser()
+            }
         }
+
+        return createNotExistingUser()
     }
 
-    suspend fun savePlayer(playerData: PlayerData) {
-        val sql = "INSERT OR REPLACE INTO players(uuid, name) VALUES(?, ?)"
-        val preparedStatement = connection.prepareStatement(sql)
+    suspend fun saveIfNotExists(player: Player) {
+        withContext(Dispatchers.IO) {
+            if(isUnknownUser(player)) {
+                return@withContext
+            }
 
-        preparedStatement.setString(1, playerData.uuid.toString())
-        preparedStatement.setString(2, playerData.name)
-        preparedStatement.executeUpdate()
+            val sql = "INSERT OR IGNORE INTO players(uuid, name) VALUES(?, ?)"
+            val preparedStatement = connection.prepareStatement(sql)
+
+            preparedStatement.setString(1, player.uniqueId.toString())
+            preparedStatement.setString(2, player.username)
+            preparedStatement.executeUpdate()
+        }
     }
 
     fun disconnect() {
         connection.close()
+
+        OfflineVelocity.instance.logger.info("OV > Disconnected from SQLite database.")
     }
 
     suspend fun getUUID(name: String): UUID? {
-        val sql = "SELECT uuid FROM players WHERE name = ?"
-        val preparedStatement = connection.prepareStatement(sql)
+        withContext(Dispatchers.IO) {
+            val sql = "SELECT uuid FROM players WHERE name = ?"
+            val preparedStatement = connection.prepareStatement(sql)
 
-        preparedStatement.setString(1, name)
+            preparedStatement.setString(1, name)
 
-        val resultSet: ResultSet = preparedStatement.executeQuery()
-        return if (resultSet.next()) {
-            UUID.fromString(resultSet.getString("uuid"))
-        } else {
-            null
+            val resultSet: ResultSet = preparedStatement.executeQuery()
+            return@withContext if (resultSet.next()) {
+                UUID.fromString(resultSet.getString("uuid"))
+            } else {
+                null
+            }
         }
+
+        return null;
+    }
+
+    private fun createNotExistingUser() : PlayerData {
+        return PlayerData(UUID.randomUUID(), "Unknown", false)
+    }
+
+    private fun isUnknownUser(player: Player) : Boolean {
+        return player.username == "Unknown"
     }
 }
