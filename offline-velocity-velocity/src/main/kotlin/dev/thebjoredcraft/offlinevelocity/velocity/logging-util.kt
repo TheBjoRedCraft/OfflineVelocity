b69ce98c48a4cb2006652d3ff9076ed827a9b330
TheBjoRedCraft <thebjoredcraft@gmail.com>
2025-03-23 01:02:16 +0100

    feat: add logging utility functions for plugin logging

---
+++ offline-velocity-velocity/src/main/kotlin/dev/thebjoredcraft/offlinevelocity/velocity/logging-util.kt
@@ -0,0 +1,17 @@
+package dev.thebjoredcraft.offlinevelocity.velocity
+
+fun log(message: String) {
+    plugin.logger.info(message)
+}
+
+fun error(message: String) {
+    plugin.logger.error(message)
+}
+
+fun warn(message: String) {
+    plugin.logger.warn(message)
+}
+
+fun info(message: String) {
+    plugin.logger.info(message)
+}
